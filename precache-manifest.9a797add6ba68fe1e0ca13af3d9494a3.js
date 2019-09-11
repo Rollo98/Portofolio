df85f61dfa9f541f31f3066f7af88e42c44a9b84
Rolleh <32469870+Rollo98@users.noreply.github.com>
2019-09-11 21:02:46 +0300

    Updates

---
+++ precache-manifest.9a797add6ba68fe1e0ca13af3d9494a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658dbd88128a84f24604c554f0c9f6aa",
+    "url": "/Portofolio/index.html"
+  },
+  {
+    "revision": "f8b2ff982094318230f6",
+    "url": "/Portofolio/static/css/main.7fb3b9c5.chunk.css"
+  },
+  {
+    "revision": "81ff2f12af3b00200c96",
+    "url": "/Portofolio/static/js/2.0fd60a1b.chunk.js"
+  },
+  {
+    "revision": "f8b2ff982094318230f6",
+    "url": "/Portofolio/static/js/main.f2f7ed01.chunk.js"
+  },
+  {
+    "revision": "9ffdf2c4ce2438e241e1",
+    "url": "/Portofolio/static/js/runtime~main.ebfb29b4.js"
+  },
+  {
+    "revision": "29e8ca9d847b5c18dddb99934f57b989",
+    "url": "/Portofolio/static/media/Ellipseleft.29e8ca9d.svg"
+  },
+  {
+    "revision": "7dc0f021d86fe7de9a12fddd3d978103",
+    "url": "/Portofolio/static/media/PC.7dc0f021.svg"
+  },
+  {
+    "revision": "13bfe11134b9d5816b643c44ba914d6c",
+    "url": "/Portofolio/static/media/Polygonright.13bfe111.svg"
+  },
+  {
+    "revision": "36dfea238c9a347fb22b7c3ca7d2edcb",
+    "url": "/Portofolio/static/media/Subtractbg.36dfea23.svg"
+  },
+  {
+    "revision": "24e26be6d52e3561927e4e54c2f90f80",
+    "url": "/Portofolio/static/media/UnionarrowGrad.24e26be6.svg"
+  }
+]);
